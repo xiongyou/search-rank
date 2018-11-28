68df3b768ca0af6eb88b642839f6f24a3336ce18
xiongyou <xiongyou@hikvision.com.cn>
2018-11-28 14:19:19 +0800

    Initialize Project

---
+++ src/main/java/com/search/searchrank/SearchRankApplication.java
@@ -0,0 +1,12 @@
+package com.search.searchrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRankApplication.class, args);
+	}
+}
